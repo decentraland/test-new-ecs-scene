927c355c588a7a773556c9f5af37906bbfc732e9
Leandro Mendoza <leandro@decentraland.org>
2022-05-02 18:30:02 -0300

    final scene

--- src/scene.ts
+++ src/scene.ts
@@ -1,28 +1,26 @@
+import type { Entity as EntityNewECSType } from '@dcl/ecs/dist/engine/entity'
 import { EngineNewECSType } from './new-ecs'
 
-export function setup(newEngine: EngineNewECSType) {
-  const sdk = newEngine.baseComponents
-  const myEntity = newEngine.addEntity()
-
-  sdk.Transform.create(myEntity, {
-    position: { x: 1, y: 2, z: 3 },
-    scale: { x: 1, y: 2, z: 3 },
-    rotation: { x: 1, y: 2, z: 3, w: Math.PI }
-  })
-
-  let t = 0.0
+/**
+ * @param periodSeconds - complete period in seconds
+ * @param update - the function to call when update
+ * @returns Function system with the logic implemented to call p/2 the update, and
+ *  not to call the other p/2
+ */
+function halfCycleUpdateSystem(
+  periodSeconds: number,
+  update: (dt: number) => void
+) {
   let updating = true
-  newEngine.addSystem((dt: number) => {
+  let t = 0.0
+
+  return (dt: number) => {
     t += dt
 
-    if (t % 10 < 5) {
+    if (t % periodSeconds < periodSeconds / 2) {
       if (!updating) {
         log('updating....')
         updating = true
-      }
-      const group = newEngine.mutableGroupOf(sdk.Transform)
-      for (const [_entity, component] of group) {
-        component.position.x = Math.cos(t)
       }
     } else {
       if (updating) {
@@ -30,5 +28,64 @@ export function setup(newEngine: EngineNewECSType) {
         updating = false
       }
     }
+
+    if (updating) {
+      update(dt)
+    }
+  }
+}
+
+function circularSystem(newEngine: EngineNewECSType) {
+  let t = 0.0
+  const sdk = newEngine.baseComponents
+  return (dt: number) => {
+    t += 2 * Math.PI * dt
+
+    const group = newEngine.groupOf(sdk.BoxShape)
+    for (const [entity] of group) {
+      const transform = sdk.Transform.mutable(entity)
+      if (transform) {
+        transform.position.x = 8 + 2 * Math.cos(t)
+        transform.position.z = 8 + 2 * Math.sin(t)
+      }
+    }
+  }
+}
+
+function createCube(
+  x: number,
+  y: number,
+  z: number,
+  newEngine: EngineNewECSType
+): EntityNewECSType {
+  const sdk = newEngine.baseComponents
+  const myEntity = newEngine.addEntity()
+
+  sdk.Transform.create(myEntity, {
+    position: { x, y, z },
+    scale: { x: 1, y: 1, z: 1 },
+    rotation: { x: 0, y: 0, z: 0, w: 1 }
   })
+
+  sdk.BoxShape.create(myEntity, {
+    withCollisions: true,
+    isPointerBlocker: true,
+    visible: true,
+    uvs: []
+  })
+
+  return myEntity
+}
+
+export function setup(newEngine: EngineNewECSType) {
+  createCube(8, 2, 8, newEngine)
+
+  // Systems
+  const completeCyclePeriodSeconds = 10
+  const mySystem = halfCycleUpdateSystem(
+    completeCyclePeriodSeconds,
+    circularSystem(newEngine)
+  )
+  newEngine.addSystem(mySystem)
+  // newEngine.addSystem(rotatorSystem(newEngine))
 }
